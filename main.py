ae7e12ca7fb0bad7f8bb3462e1677f2a68bd6d3d
JuanDavidMartinez <41515843+JuanDavidMartinez@users.noreply.github.com>
2025-08-20 16:37:21 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import psycopg2
+import os
+
+app = FastAPI()
+
+# URL de conexión desde variables de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Conexión
+conn = psycopg2.connect(DATABASE_URL)
+
+@app.get("/")
+def read_root():
+    return {"message": "API conectada a PostgreSQL en Neon 🚀"}
+
+@app.get("/users")
+def get_users():
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM users;")  # asume que existe tabla users
+    rows = cur.fetchall()
+    cur.close()
+    return {"users": rows}
